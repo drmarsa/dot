0e6d97009502a3915061b279ebf270596e9eac76
Dennis Marsa <dennis.marsa@gmail.com>
2022-02-21 15:06:57 -0700

    adds more

---
+++ .emacs.d/init.el
@@ -0,0 +1,24 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(column-number-mode nil)
+ '(custom-enabled-themes '(tango-dark))
+ '(display-time-mode t)
+ '(fringe-mode 15 nil (fringe))
+ '(global-display-line-numbers-mode t)
+ '(inhibit-startup-screen t)
+ '(initial-scratch-message nil)
+ '(line-number-mode nil)
+ '(make-backup-files nil)
+ '(menu-bar-mode nil)
+ '(show-paren-mode t)
+ '(size-indication-mode t)
+ '(tool-bar-mode nil))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:family "Source Code Pro" :foundry "ADBO" :slant normal :weight normal :height 90 :width normal)))))
