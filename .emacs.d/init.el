187c3fecb2d48fab81fe09f70749f4ce8e33b404
Dennis Marsa <dennis.marsa@gmail.com>
2023-03-31 15:24:11 -0600

    emacs init updates

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,3 +1,64 @@
+;***********************************************************************
+; .emacs for Dennis Marsa
+;***********************************************************************
+
+;***********************************************************************
+; Add my emacs library to the load path
+;***********************************************************************
+(setq load-path (cons (concat "/home/" user-login-name "/emacs") load-path))
+
+;***********************************************************************
+; Show the time on the mode line
+;***********************************************************************
+(display-time)
+
+;***********************************************************************
+; Show matching parentheses next to cursor
+;***********************************************************************
+(require `paren)
+(show-paren-mode t)
+
+;***********************************************************************
+; Various global variable settings
+;***********************************************************************
+(setq inhibit-startup-message t)
+(setq make-backup-files nil)
+(setq require-final-newline t)
+(setq-default tab-width 8)
+(setq-default truncate-lines t)
+(setq-default truncate-partial-width-windows nil)
+(setq-default indent-tabs-mode nil)  ; convert tabs to spaces
+(setq-default display-line-numbers t)
+
+;***********************************************************************
+; My own key bindings
+;***********************************************************************
+(global-set-key "l" 'goto-line)
+(global-set-key "" 'isearch-forward-regexp)
+(global-set-key "" 'isearch-backward-regexp)
+(global-set-key "" 'compile)
+(global-set-key "" 'compile)
+(global-set-key "" 'shell)
+(global-set-key "\M-s" 'compare-windows)
+
+;***********************************************************************
+; I want ^L to put the line I'm on at the top of the window
+;***********************************************************************
+(defun my-recenter () (interactive) (recenter 0))
+(global-set-key "" 'my-recenter)
+
+;***********************************************************************
+; I accidentally type ^X^C sometimes, guard against that
+;***********************************************************************
+(global-set-key "" 'my-quit)
+(defun my-quit () (interactive)
+  (if (y-or-n-p '"Really quit? ") (save-buffers-kill-emacs)))
+
+;***********************************************************************
+; I know how to use narrow-to-region, so enable it
+;***********************************************************************
+(put 'narrow-to-region 'disabled nil)
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
